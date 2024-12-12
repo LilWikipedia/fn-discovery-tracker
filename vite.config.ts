fae43a5bcdb5b8fe40e43ea07686974c4c92603e
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-12-12 16:36:28 +0000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
-
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  plugins: [react()].filter(Boolean),
+  base: '/fn-discovery-tracker/',
   server: {
+    host: "::",
+    port: 8080,
     proxy: {
       '/api': 'http://localhost:3001',
-    },
+    }
   },
-});
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
+}));
