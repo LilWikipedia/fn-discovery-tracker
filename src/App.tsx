34e2f87cb35981b4ce95ffaf3a216b0ff6378f5b
Cruz Wootten <78571191+LilWikipedia@users.noreply.github.com>
2024-12-12 00:04:57 +0000

    k

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
@@ -14,6 +15,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
